168e69e8f571827b249bffb8d55ed77a28484f36
Brandon Barrington <bbarrington0099@gmail.com>
2024-12-09 21:50:21 -0600

    Turned off watch

--- refresh-repo.sh
+++ refresh-repo.sh
@@ -14,12 +14,16 @@ git fetch origin main >> "$LOG_FILE" 2>&1
 echo "$CURRENT_DATE - Git fetch completed" >> "$LOG_FILE"
 
 # Sleep to ensure proper Git reset and app stability
-sleep 10
+sleep 2
 
 # Perform the reset step
 echo "$CURRENT_DATE - Resetting local branch to match remote..." >> "$LOG_FILE"
 git reset --hard origin/main >> "$LOG_FILE" 2>&1
 echo "$CURRENT_DATE - Git reset completed" >> "$LOG_FILE"
+
+echo "Restarting App" >> "$LOG_FILE"
+echo $(pm2 restart mohc-web) >> "$LOG_FILE"
+echo "App Restarted" >> "$LOG_FILE"
 
 echo "$CURRENT_DATE - Repo Fresh" >> "$LOG_FILE"
 echo "" >> "$LOG_FILE"
