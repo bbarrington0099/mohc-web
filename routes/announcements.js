7e6b1f88328e1fc82d26803f0908b40a76133b3a
Brandon Barrington <bbarrington0099@gmail.com>
2024-12-10 20:21:59 -0600

    test

--- routes/announcements.js
+++ routes/announcements.js
@@ -2,13 +2,13 @@ const express = require('express');
 const pool = require('../config/db');
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+/* router.get('/', async (req, res) => {
     try {
         const [rows] = await pool.query('SELECT * FROM announcements');
         res.json(rows);
     } catch (err) {
         res.status(500).send('Error fetching announcements');
     }
-});
+}); */
 
 module.exports = router;
