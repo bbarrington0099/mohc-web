bdcab1e2e13ae7b5f6d4c6adbd8e726a3e281537
Brandon Barrington <bbarrington0099@gmail.com>
2024-12-19 22:48:25 -0600

    In Progress src for iFrame, mobile updates, font, autoscroll

--- public/script.js
+++ public/script.js
@@ -1,6 +1,26 @@
 const mobileMenuButton = document.getElementById('mobile-menu-button');
 const pageSideContainer = document.getElementById('page-side');
 const navOptions = document.getElementsByClassName('nav-option');
+const contentFrame = document.getElementById('mainContent');
+
+const initialize = () => {
+    contentFrame.src = `./InProgress/index.html`;
+    
+    for (let navOption of navOptions) {
+        navOption.addEventListener('click', (event) => {
+            toggleMobileMenu();
+            
+            for (let navOption of navOptions) {
+                navOption.classList.remove('selected');
+                navOption.removeAttribute('disabled');
+            }
+
+            event.target.classList.add('selected');
+            event.target.setAttribute('disabled', 'true');
+            contentFrame.src = `./${event.target.getAttribute('data-dir')}/index.html`;
+        })
+    }
+}
 
 toggleMobileMenu = () => {
     pageSideContainer.classList.toggle('opened');
@@ -9,12 +29,4 @@ toggleMobileMenu = () => {
     mobileMenuButton.classList.toggle('closed');
 }
 
-for (let navOption of navOptions) {
-    navOption.addEventListener('click', (event) => {
-        toggleMobileMenu();
-        for (let navOption of navOptions) {
-            navOption.classList.remove('selected');
-        }
-        event.target.classList.add('selected');
-    })
-}
+document.addEventListener("DOMContentLoaded", initialize);
